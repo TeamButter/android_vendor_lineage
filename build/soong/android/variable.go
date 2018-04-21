6340e327a63a0a246af7407c6e6ed33ba4885940
Aaron Kling <webgeek1234@gmail.com>
2018-04-21 11:37:39 -0500

    Add Nvidia enhancements soong flag
    Change-Id: I1bec17d2457944350dcb5a22c6d571d3ace913a7

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -27,6 +27,9 @@ type Product_variables struct {
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
 	}
+	Uses_nvidia_enhancements struct {
+		Cppflags []string
+	}
 	Uses_qti_camera_device struct {
 		Cppflags []string
 		Shared_libs []string
@@ -43,6 +46,8 @@ type ProductVariables struct {
 	Needs_text_relocations  *bool `json:",omitempty"`
 	Uses_media_extensions  *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
 }
